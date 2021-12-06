7e9c47bbf0ed18dccbae0897df43865089bca196
ImStif <93940957+ImStif@users.noreply.github.com>
2021-12-06 19:52:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>Panel<sup>v.2.28</sup></title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
